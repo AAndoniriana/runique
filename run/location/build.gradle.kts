074fa4e821385da8ba85cb68484eb4646208fd81
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2024-05-20 14:59:54 +0300

    Define module relation

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -33,9 +33,17 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.google.android.gms.play.services.location)
-    implementation(libs.androidx.work)
-    implementation(libs.koin.android.workmanager)
-    implementation(libs.kotlinx.serialization.json)
+
+    implementation(projects.core.domain)
+    implementation(projects.run.domain)
 }
