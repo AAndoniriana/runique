a20d6b6e550dc4f0a25a70c7ba4a56c540310c32
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2024-05-29 16:36:22 +0300

    Create JvmKtorConventionPlugin

---
+++ build-logic/convention/src/main/java/JvmKtorConventionPlugin.kt
@@ -0,0 +1,19 @@
+import mg.jaava.convention.configureKotlinJvm
+import mg.jaava.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+
+class JvmKtorConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply("org.jetbrains.kotlin.plugin.serialization")
+            configureKotlinJvm()
+
+            dependencies {
+                "implementation"(libs.findBundle("ktor").get())
+            }
+        }
+    }
+}
