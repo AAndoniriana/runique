22118786cb92e0b797d671090d5f0d045987412d
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2024-07-18 13:33:12 +0300

    Setting up the ActiveRunViewModel

--- run/presentation/src/main/java/mg/jaava/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/mg/jaava/run/presentation/active_run/ActiveRunViewModel.kt
@@ -3,15 +3,18 @@ package mg.jaava.run.presentation.active_run
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import mg.jaava.run.domain.RunningTracker
-import timber.log.Timber
 
 class ActiveRunViewModel(
     private val runningTracker: RunningTracker
@@ -22,10 +25,18 @@ class ActiveRunViewModel(
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    private val _hasLocationPermission = MutableStateFlow(false)
+    private val shouldTrack = snapshotFlow { state.shouldTrack }
+        .stateIn(viewModelScope, SharingStarted.Lazily, state.shouldTrack)
+    private val hasLocationPermission = MutableStateFlow(false)
+    private val isTracking = combine(
+        shouldTrack,
+        hasLocationPermission
+    ) { shouldTrack, hasLocationPermission ->
+        shouldTrack && hasLocationPermission
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
 
     init {
-        _hasLocationPermission
+        hasLocationPermission
             .onEach { hasPermission ->
                 if (hasPermission) {
                     runningTracker.startObservingLocation()
@@ -35,24 +46,47 @@ class ActiveRunViewModel(
             }
             .launchIn(viewModelScope)
 
-        runningTracker.currentLocation
-            .onEach { location ->
-                Timber.d("Location: $location")
+        isTracking
+            .onEach { isTracking ->
+                runningTracker.setIsTracking(isTracking)
             }
             .launchIn(viewModelScope)
+
+        runningTracker
+            .currentLocation
+            .onEach {
+                state = state.copy(currentLocation = it?.location)
+            }
+            .launchIn(viewModelScope)
+
+        runningTracker
+            .runData
+            .onEach {
+                state = state.copy(runData = it)
+            }
+            .launchIn(viewModelScope)
+
+        runningTracker
+            .elapsedTime
+            .onEach {
+                state = state.copy(elapsedTime = it)
+            }
     }
 
     fun onAction(action: ActiveRunAction) {
         when (action) {
             is ActiveRunAction.OnToggleRunClick -> {
                 state = state.copy(
+                    hasStartedRunning = true,
                     shouldTrack = !state.shouldTrack
                 )
             }
             ActiveRunAction.OnFinishRunClick -> {}
-            ActiveRunAction.OnResumeRunClick -> {}
+            ActiveRunAction.OnResumeRunClick -> {
+                state = state.copy(shouldTrack = true)
+            }
             is ActiveRunAction.SubmitLocationPermissionInfo -> {
-                _hasLocationPermission.value = action.acceptedLocationPermission
+                hasLocationPermission.value = action.acceptedLocationPermission
                 state = state.copy(
                     showLocationRational = action.showLocationRational
                 )
@@ -68,7 +102,9 @@ class ActiveRunViewModel(
                     showNotificationRational = false
                 )
             }
-            else -> Unit
+            ActiveRunAction.OnBackClick -> {
+                state = state.copy(shouldTrack = false)
+            }
         }
     }
 }
